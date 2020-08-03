7b2a71c1457aec5441dd796d36357eda11feff23
Secozzi <folke.steen85@gmail.com>
2020-08-03 23:05:10 +0200

    added support for brackets

--- example.py
+++ example.py
@@ -11,8 +11,8 @@ editor.setStyleSheet("""QPlainTextEdit{font: 8pt "Courier New"; color: #383a42; 
 highlight = syntax_pars.PythonHighlighter(editor.document())
 editor.show()
 
-# Load syntax.py into the editor for demo purposes
-text = open("../CASPy3/caspy/qt_assets/main.py", "r")
+# Load paren_highlight into the editor for demo purposes
+text = open("paren_highlight.py", "r")
 editor.setPlainText(text.read())
 
 editor.setFixedSize(800, 800)
--- paren_highlight.py
+++ paren_highlight.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QSyntaxHighlighter, QTextBlockUserData, QTextCursor
 from PyQt5.QtWidgets import QPlainTextEdit, QTextEdit
 
 ParenInfo = namedtuple("ParenInfo", "character position")
+BracketInfo = namedtuple("BracketInfo", "character position")
 
 
 class ParenMatchHighlighter(QSyntaxHighlighter):
@@ -13,6 +14,8 @@ class ParenMatchHighlighter(QSyntaxHighlighter):
         for pos, rune in enumerate(text):
             if rune in "()":
                 block_data.parentheses.append(ParenInfo(rune, pos))
+            if rune in "[]":
+                block_data.brackets.append(BracketInfo(rune, pos))
         self.setCurrentBlockUserData(block_data)
 
 
@@ -20,6 +23,7 @@ class TextBlockData(QTextBlockUserData):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.parentheses = []
+        self.brackets = []
 
 
 class TextEdit(QPlainTextEdit):
@@ -36,6 +40,7 @@ class TextEdit(QPlainTextEdit):
         if data is not None:
             abspos = self.textCursor().position()
             pos = block.position()
+
             for i, info in enumerate(data.parentheses):
                 curpos = abspos - pos
                 if info.position == curpos - 1 and info.character == "(":
@@ -45,6 +50,19 @@ class TextEdit(QPlainTextEdit):
                         self.highlightIncomplete(pos + info.position)
                 if info.position == curpos - 1 and info.character == ")":
                     if self.matchRightPar(block, i):
+                        self.createParSelection(pos + info.position)
+                    else:
+                        self.highlightIncomplete(pos + info.position)
+
+            for i, info in enumerate(data.brackets):
+                curpos = abspos - pos
+                if info.position == curpos - 1 and info.character == "[":
+                    if self.matchLeftParBracket(block, i + 1):
+                        self.createParSelection(pos + info.position)
+                    else:
+                        self.highlightIncomplete(pos + info.position)
+                if info.position == curpos - 1 and info.character == "]":
+                    if self.matchRightParBracket(block, i):
                         self.createParSelection(pos + info.position)
                     else:
                         self.highlightIncomplete(pos + info.position)
@@ -82,6 +100,43 @@ class TextEdit(QPlainTextEdit):
         block = block.previous()
         if block.isValid():
             return self.matchRightPar(block, None, count)
+        else:
+            self.highlightIncomplete(self.textCursor().position())
+            return False
+
+    def matchLeftParBracket(self, block, index, count=0):
+        pos = block.position()
+        data = block.userData()
+        for i, info in enumerate(data.brackets[index:]):
+            if info.character == "[":
+                count += 1
+            elif info.character == "]":
+                if count == 0:
+                    self.createParSelection(pos + info.position)
+                    return True
+                else:
+                    count -= 1
+        block = block.next()
+        if block.isValid():
+            return self.matchLeftParBracket(block, 0, count)
+        else:
+            return False
+
+    def matchRightParBracket(self, block, index, count=0):
+        pos = block.position()
+        data = block.userData()
+        for i, info in enumerate(reversed(data.brackets[:index])):
+            if info.character == "]":
+                count += 1
+            elif info.character == "[":
+                if count == 0:
+                    self.createParSelection(pos + info.position)
+                    return True
+                else:
+                    count -= 1
+        block = block.previous()
+        if block.isValid():
+            return self.matchRightParBracket(block, None, count)
         else:
             self.highlightIncomplete(self.textCursor().position())
             return False
